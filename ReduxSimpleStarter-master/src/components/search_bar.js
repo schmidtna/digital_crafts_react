7fe26dc2f711e2cf9dd6352317598821e458823b
Nick Schmidt <nick@schmidtyanimation.com>
2018-06-14 20:48:57 -0500

    added input change handler

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input />
+        return <input onChange={this.onInputChange} />;
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+
     }
 }
+
+//refactorable : return <input onChange ={event => console.log(event.target.value)} />;
+//then can remove the entire event handler function
 
 
 export default SearchBar;
