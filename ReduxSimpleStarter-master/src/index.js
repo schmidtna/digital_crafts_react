e2b11a49724bdfc3ad8bfaa41fd12af8e06bf912
Nick Schmidt <nick@schmidtyanimation.com>
2018-06-14 19:33:25 -0500

    working on udemy react youtube clone app

---
+++ ReduxSimpleStarter-master/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a new component, this component produces html
+
+const App = () => <div>Hi!</div>;
+
+// take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
