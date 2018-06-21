748ae49ddb62733b5eb1809c61fd66cd7d241747
Nick Schmidt <nick@schmidtyanimation.com>
2018-06-21 06:01:12 -0500

    set config var and setting search class

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 
 import SearchBar from './components/search_bar';
 
-// const config = require('../config.js');
-// const youTubeApiKey = config.API_KEY;
+const config = require('../config');
 
 
 // // create a new component, this component produces html
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = { videos: []};
+
+        YTSearch({key: config.yt.api_key, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            // this.setState ({ videos: videos }); es6 condensed above
+        });
+    }
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 // // take this component's generated HTML and put it on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
