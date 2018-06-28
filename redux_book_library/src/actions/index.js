5b3a76a5f6a3afe4d4489d05b77bcd78df5d6ec0
Nick Schmidt <nick@schmidtyanimation.com>
2018-06-28 18:13:55 -0500

    renamed redux intro directory

--- redux_book_library/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export function selectBook(book) {
-// selectBook is an Action Creator, needs to return and action 
-// which is an object with a type property
-return {
-    type: 'BOOK_SELECTED',
-    payload: book
-};
-}
-
--- redux_book_library/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-    return [
-        {title: 'Javascript: The Good Parts', pages: 123 },
-        {title: 'Harry Potter', pages: 45 },
-        {title: 'The dark tower', pages: 238},
-        {title: 'Eloquent Ruby', pages: 1 }
-    ]
-};
