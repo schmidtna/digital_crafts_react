f6417dfea5c9e537359bb2dcde4c513adf7c05cd
Nick Schmidt <nick@schmidtyanimation.com>
2018-06-30 11:23:48 -0500

    added multiple charts and google maps

--- redux_weather_app/src/actions/index.js
+++ redux_weather_app/src/actions/index.js
@@ -9,11 +9,9 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${config
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},us`;
+    const url = `${ROOT_URL}&q=${city}&units=imperial`;
     const request = axios.get(url);
 
-    console.log('Request:', request);
-    
     return {
         type: FETCH_WEATHER,
         payload: request
