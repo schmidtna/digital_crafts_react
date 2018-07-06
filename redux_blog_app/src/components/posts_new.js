39792ffe0540049fabd97ad4af7417637eba94e7
Nick Schmidt <nick@schmidtyanimation.com>
2018-07-05 21:06:01 -0500

    added input fields and generalized the code for them

--- redux_blog_app/src/components/posts_new.js
+++ redux_blog_app/src/components/posts_new.js
@@ -2,13 +2,44 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+renderField(field) {
+    return (
+        <div className="form-group">
+            <label>{field.label}</label>
+            <input
+                className="form-control"
+                type="text" 
+                {...field.input}
+                //this passes in all pre generated event handlers like onChange, onFocus etc., all at once
+            />
+        </div>
+    )
+
+}
+
     render() {
         return (
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
